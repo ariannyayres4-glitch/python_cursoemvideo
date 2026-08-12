9ec357ceeb19cf5b9ad37a4ea3ff6d5d73e3efc9
ariannyayres4-glitch <ariannyayres4@gmail.com>
2026-08-12 15:58:30 -0300

    inicio do exercícios da aula de modulo

--- Python/Curso-Python/modulo.py
+++ Python/Curso-Python/modulo.py
@@ -1,8 +1,7 @@
-import random, emoji
+import random
 from math import sqrt,trunc
 num = int(input('Digite um número: '))
 raiz = sqrt(num)
-print(f'A raiz de {num} é {trunc(raiz)}')
-
+print(f'A raiz de {num} é {trunc(raiz)}') 
 num = random.randint(66666666665 ,66666666666 )
 print(num)
---
+++ Python/Exercicios_python/ex012.py
@@ -0,0 +1,3 @@
+from math import trunc
+n1 = float(input('Digite um número: '))
+print(f'A parrte inteira de {n1} é {trunc(n1)}')
